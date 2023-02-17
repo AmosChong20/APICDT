4db5dc295e38dd067b2e56238bd5d83a59791783
AmosChong <amoschong0820@gmail.com>
2023-02-17 20:54:50 +0800

    lALA

--- client/components/form.js
+++ client/components/form.js
@@ -132,7 +132,7 @@ function Form({ information }) {
             body: JSON.stringify({
                 email: leaderEmail,
                 password: password,
-                username: 'abcdefg',
+                username: leaderNameEN,
                 userRole: 'Participant',
                 school: schoolNameCN
             }),
