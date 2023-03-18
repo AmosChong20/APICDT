11c29f6cce80dfd37b4bd5b33b05fd48e69d1a08
AmosChong <amoschong0820@gmail.com>
2023-03-19 00:00:51 +0800

    Modified title

--- client/components/header.js
+++ client/components/header.js
@@ -10,7 +10,7 @@ function Header({ title }) {
             <Image alt='apicdt-background' src={require('../public/logo/newBanner.png')} priority fill className={styles.image} />
             <Flex w='60vw' h='15.36vh' align={'center'} justify='center' flexDir={'column'} className={styles.header}>
                 <Flex fontFamily={'ZCOOL XiaoWei'} className={styles.chinHeader} letterSpacing={'0.04em'} justify='center' color='#FFDCB0'>{title}</Flex>
-                <Flex fontFamily={'ZCOOL XiaoWei'} className={styles.engHeader} letterSpacing={'0.04em'} justify='center' color='#FFBD68'>11th Asia-Pacific Intensity Chinese Debate Tournament</Flex>
+                <Flex fontFamily={'ZCOOL XiaoWei'} className={styles.engHeader} letterSpacing={'0.04em'} justify='center' color='#FFBD68'>11th Asia-Pacific Intervarsity Chinese Debate Tournament</Flex>
             </Flex>
         </Flex >
         // </div>
