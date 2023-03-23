0bd8852b0496bccac9d8063c1540bd4b09f3a6bd
AmosChong <amoschong0820@gmail.com>
2023-03-23 20:17:21 +0800

    Upgraded starwars

--- client/components/drawnResultsArea.js
+++ client/components/drawnResultsArea.js
@@ -1,16 +1,19 @@
 import { Flex, Heading, ListItem, OrderedList } from '@chakra-ui/react'
 import '@fontsource/ma-shan-zheng'
+import '@fontsource/zcool-xiaowei'
 
 function DrawnResultsArea({ data, area }) {
     const areaFilter = data.filter(result => result.attributes.area == area)
+    const areaSort = areaFilter.sort((a, b) => a.attributes.timeUsed - b.attributes.timeUsed)
+    const schoolFilter = areaSort.filter((school, index) => areaSort.findIndex(school2 => school.attributes.schoolName == school2.attributes.schoolName ) == index)
     
     return ( 
-        <Flex fontFamily={'Ma Shan Zheng'} mt={10} ml={10} h='92vh' flexDirection={'column'}>
-            <Heading fontFamily={'Ma Shan Zheng'} mb={5}>{`抽签结果（地区：${area}）`}</Heading>
-            <OrderedList>{areaFilter.map((result) => {
+        <Flex fontFamily={'ZCOOL XiaoWei'} mt={10} ml={10} h='92vh' fontSize={'45px'} flexDirection={'column'}>
+            <Heading fontSize={'55px'} fontFamily={'ZCOOL XiaoWei'} mb={5}>{`抽签结果（地区：${area}）`}</Heading>
+            <OrderedList>{schoolFilter.map((result) => {
                 const { attributes } = result
                 return (
-                    <ListItem fontSize={20} key={result.id}>
+                    <ListItem fontSize={'35px'} key={result.id}>
                         {`${attributes.schoolName} :   ${attributes.timeUsed}秒`}
                     </ListItem>
                 )
--- client/pages/starwars.js
+++ client/pages/starwars.js
@@ -20,6 +20,7 @@ function Starwars({ initialTime }) {
     const [date, setDate] = useState(new Date())
     const [submitted, setSubmitted] = useState(false)
     const [notTimeYet, setNotTimeYet] = useState(false)
+    const [afterTime, setAfterTime] = useState(false)
     const [showSuccessAlert, setShowSuccessAlert] = useState(false)
     const [showFailAlert, setShowFailAlert] = useState(false)
     const [showTimeAlert, setTimeAlert] = useState(false)
@@ -28,13 +29,13 @@ function Starwars({ initialTime }) {
     const [selectedArea, setSelectedArea] = useState()
     const router = useRouter()
 
-    useEffect(() => {
-        const timerForTime = setInterval(() => setDate(Date.now()), 1000)
+    // useEffect(() => {
+    //     const timerForTime = setInterval(() => setDate(Date.now()), 1000)
         
-        return () => {
-            clearInterval(timerForTime)
-        }
-    }, [])
+    //     return () => {
+    //         clearInterval(timerForTime)
+    //     }
+    // }, [])
 
     // useEffect(() => {
     //     if (new Date() < new Date('2023-03-17T17:00:00')) {
@@ -45,11 +46,11 @@ function Starwars({ initialTime }) {
     //     }
     // }, [])
 
-    if (!date) {
-        return (
-            <Loading />
-        )
-    }
+    // if (!date) {
+    //     return (
+    //         <Loading />
+    //     )
+    // }
 
     if (!session)
         return (
@@ -74,7 +75,7 @@ function Starwars({ initialTime }) {
     }
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
         // if (!area) {
         //     setShowFailAlert(true)
@@ -86,13 +87,23 @@ function Starwars({ initialTime }) {
         if (submitted) {
             return null
         }
+        setSubmitted(true)
         setTimeout(() => {
             setSubmitted(false)
         }, 5000)
-        setSubmitted(true)
-        const endTime = date
+        const clickedTime = Date.now()
         const startTime = new Date(selectedArea[0].startTime)
-        const newDuration = endTime - startTime.getTime()
+        const newDuration = clickedTime - startTime.getTime()
+        const endTime = new Date(selectedArea[0].endTime)
+
+        if (clickedTime > endTime.getTime()) {
+            setAfterTime(true)
+            setSubmitted(false)
+            setTimeout(() => {
+                setAfterTime(false)
+            }, 500)
+            return null
+        }
 
         if (newDuration < 0) {
             setTimeAlert(true)
@@ -122,18 +133,17 @@ function Starwars({ initialTime }) {
             const schoolRes = await schoolResponse.json()
 
             if (schoolRes.data.length > 0) {
-                setRepeatAlert(true)
-                setTimeout(() => {
-                    setRepeatAlert(false)
-                }, 2000)
+                // setRepeatAlert(true)
+                // setTimeout(() => {
+                //     setRepeatAlert(false)
+                // }, 2000)
                 return null
             }
-            setShowSuccessAlert(true)
+
+            // setShowSuccessAlert(true)
             // setTimeout(() => {
             //     setShowSuccessAlert(false)
-            //     router.push(`/drawnResults/${area}`)
             // }, 2000)
-            router.push(`/drawnResults/${area}`)
             const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}drawn-results`, {
                 method: 'POST',
                 body: JSON.stringify({
@@ -147,8 +157,9 @@ function Starwars({ initialTime }) {
                     'Content-type': 'application/json'
                 }
             })
-            console.log(response)
             const res = await response.json()
+            console.log(res)
+            router.push(`/drawnResults/${area}`)
             const { data } = res
         }
         catch (e) {
@@ -169,33 +180,40 @@ function Starwars({ initialTime }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            {submitted && <Loading/> }
             {notTimeYet && <Flex justify={'center'} mt={'70px'} ml={'-15px'} fontFamily={'ZCOOL XiaoWei'} fontSize={'26px'} h={'92vh'} color='black'>电子抽签系统暂未开放！</Flex>}
             {!notTimeYet &&
-                <Flex fontFamily={'ZCOOL XiaoWei'} h={'92vh'} align='center' flexDirection={'column'}>
+                <Flex fontFamily={'ZCOOL XiaoWei'} h={'92vh'} align='center' justify={'center'} flexDirection={'column'}>
                     {selectedArea ?
-                        <Stack align='center' mt={'100px'}>
-                            {showSuccessAlert ? <Flex justify={'center'} position='absolute' mt='-400px'>
+                        <Stack align='center' justify={'center'}>
+                            {showSuccessAlert ? <Flex justify={'center'} position='absolute' mt={'40px'} fontSize={'26px'}>
                                 <Alert status='success' zIndex={1} color={'black'} w={'50vw'}>
                                     <AlertIcon />
-                                    <AlertTitle>成功提交！页面将于5秒后跳转！</AlertTitle>
+                                    <AlertTitle>成功提交！页面将于2秒后跳转！</AlertTitle>
                                 </Alert>
                             </Flex> : <Box></Box>}
-                            {showRepeatAlert ? <Flex justify={'center'} position='absolute' mt='-400px'>
+                            {showRepeatAlert ? <Flex justify={'center'} position='relative' mt='-450px' fontSize={'26px'}>
                                 <Alert status='error' zIndex={1} color={'black'} w={'50vw'}>
                                     <AlertIcon />
                                     <AlertTitle>此队伍已提交！</AlertTitle>
                                 </Alert>
                             </Flex> : <Box></Box>}
-                            {showFailAlert ? <Flex justify={'center'} position='absolute' mt='-400px'>
+                            {showFailAlert ? <Flex justify={'center'} position='relative' mt='-450px' fontSize={'26px'}>
                                 <Alert status='error' zIndex={1} color={'black'} w={'50vw'} >
                                     <AlertIcon />
                                     <AlertTitle>请选择地区！</AlertTitle>
                                 </Alert>
                             </Flex> : <Box></Box>}
-                            {showTimeAlert ? <Flex justify={'center'} position='absolute' mt='-400px'>
+                            {showTimeAlert ? <Flex justify={'center'} position='relative' mt='-450px' fontSize={'26px'}>
                                 <Alert status='error' zIndex={1} color={'black'} w={'50vw'}>
                                     <AlertIcon />
                                     <AlertTitle>还未到抽签时间！</AlertTitle>
+                                </Alert>
+                            </Flex> : <Box></Box>}
+                            {afterTime ? <Flex justify={'center'} position='relative' mt='-450px' fontSize={'26px'}>
+                                <Alert status='error' zIndex={1} color={'black'} w={'50vw'}>
+                                    <AlertIcon />
+                                    <AlertTitle>抽签已结束！</AlertTitle>
                                 </Alert>
                             </Flex> : <Box></Box>}
                             <Select borderColor={'Black'} w='150px' placeholder='地区' value={area} onChange={handleAreaChange} zIndex={0}>
@@ -205,10 +223,10 @@ function Starwars({ initialTime }) {
                                     )
                                 })}
                             </Select>
-                            <Stack align='center'>
-                                <Heading fontFamily={'ZCOOL XiaoWei'} fontSize={'54px'}>{`${selectedArea[0].area}`} </Heading>
-                                <Heading fontFamily={'ZCOOL XiaoWei'}>{`开始抽签时间：${moment(selectedArea[0].startTime).format("D/M/yyyy hh:mm a")}`} </Heading>
-                                <Heading fontFamily={'ZCOOL XiaoWei'}>{`结束抽签时间：${moment(selectedArea[0].endTime).format("D/M/yyyy hh:mm a")}`} </Heading>
+                            <Stack align={'center'} gap="15px">
+                                <Heading fontFamily={'ZCOOL XiaoWei'} fontSize={'75px'}>{`${selectedArea[0].area}`} </Heading>
+                                <Heading fontFamily={'ZCOOL XiaoWei'} mt={'20px'}>{`开始抽签时间：${moment(selectedArea[0].startTime).format("D/M/yyyy hh:mm a")}`} </Heading>
+                                <Heading fontFamily={'ZCOOL XiaoWei'} mt={'20px'}>{`结束抽签时间：${moment(selectedArea[0].endTime).format("D/M/yyyy hh:mm a")}`} </Heading>
                                 {/* <Heading fontFamily={'ZCOOL XiaoWei'} fontSize={138} mb={10}>{moment(date).format("hh:mm:ss a")}</Heading> */}
                                 <Button fontSize={'65px'} p={10} mb={20} onClick={handleSubmit}>提交</Button></Stack> </Stack> : <Stack align='center' mt={'100px'}>
                             <Heading fontFamily={'ZCOOL XiaoWei'} fontSize={'100px'} mb={'50px'}>电子抽签系统</Heading>
