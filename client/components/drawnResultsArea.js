dae97c6f9c2ba64f7ef34747b7e506b11333f73d
AmosChong <amoschong0820@gmail.com>
2023-04-02 13:11:37 +0800

    Redesigned drawn results

--- client/components/drawnResultsArea.js
+++ client/components/drawnResultsArea.js
@@ -1,6 +1,7 @@
 import { Flex, Heading, ListItem, OrderedList } from '@chakra-ui/react'
 import '@fontsource/ma-shan-zheng'
 import '@fontsource/zcool-xiaowei'
+import styles from "../styles/drawnResultsArea.module.css"
 
 function DrawnResultsArea({ data, area }) {
     const areaFilter = data.filter(result => result.attributes.area == area)
@@ -8,16 +9,16 @@ function DrawnResultsArea({ data, area }) {
     const schoolFilter = areaSort.filter((school, index) => areaSort.findIndex(school2 => school.attributes.schoolName == school2.attributes.schoolName ) == index)
     
     return ( 
-        <Flex fontFamily={'ZCOOL XiaoWei'} mt={10} ml={10} h='92vh' fontSize={'45px'} flexDirection={'column'}>
-            <Heading fontSize={'55px'} fontFamily={'ZCOOL XiaoWei'} mb={5}>{`抽签结果（地区：${area}）`}</Heading>
-            <OrderedList>{schoolFilter.map((result) => {
+        <Flex fontFamily={'ZCOOL XiaoWei'} mt={'20px'} ml={'20px'} h='92vh' fontSize={'45px'} flexDirection={'column'}>
+            <div className={styles.title}>{`抽签结果（地区：${area}）`}</div>
+            <ol className={styles.wholeList}>{schoolFilter.map((result) => {
                 const { attributes } = result
                 return (
-                    <ListItem fontSize={'35px'} key={result.id}>
+                    <li className={styles.list} key={result.id}>
                         {`${attributes.schoolName} :   ${attributes.timeUsed}秒`}
-                    </ListItem>
+                    </li>
                 )
-            })}</OrderedList>
+            })}</ol>
         </Flex>
      );
 }
