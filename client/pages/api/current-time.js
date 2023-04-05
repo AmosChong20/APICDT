be82ac1fcb8ef272c2fd59a8b218121d97928c20
AmosChong <amoschong0820@gmail.com>
2023-04-06 01:38:19 +0800

    Trial 5

--- client/pages/api/current-time.js
+++ client/pages/api/current-time.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     const startTime = new Date('2023-04-06T20:00:00');
     const clickedTime = new Date();
     const duration = clickedTime - startTime;
-    // console.log(clickedTime, startTime);
+    console.log(clickedTime, startTime);
     // console.log(duration);
     res.status(200).json({ duration });
 }
